ea299286eae32163d4f4d1fc97623f5f0f310bb4
damhiya <damhiya@gmail.com>
2024-12-17 09:52:26 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "logos";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        agda = pkgs.agda.withPackages (
+          pkgs: with pkgs; [
+            cubical
+          ]
+        );
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [ agda ];
+        };
+      }
+    );
+}
